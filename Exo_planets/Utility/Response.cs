978f68511078f115bc248d7c6e2275bdb5ebc4aa
haroldBauti <haroldbautistaa14@gmail.com>
2025-10-02 20:58:19 -0500

    nasa controller update

--- Exo_planets/Utility/Response.cs
+++ Exo_planets/Utility/Response.cs
@@ -3,8 +3,12 @@
     public class Response
     {
         //public bool status {  get; set; }
-        public string? output { get; set; }
         public string? url { get; set; }
+        public string? name_exoplaneta { get; set; }
+        public string? yearDisc { get; set; }
+        public string? met_desc { get; set; }
+        public string? temp { get; set; }
+        public string? ratio { get; set; }
         public string? Value { get; set; }
     }
 }
